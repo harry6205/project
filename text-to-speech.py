8faeb2f8e2be7c3348d3b48c0035dc864729d10b
harry6205 <102344087+harry6205@users.noreply.github.com>
2024-06-27 07:34:18 +0530

    text-to-speech.py

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,4 +1,4 @@
-
+#using google text to speech
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import PyPDF2
@@ -25,37 +25,46 @@ def convert_to_audio():
             text = file.read()
     else:
         return messagebox.showerror('error', 'unsupport file format!')
-
+#convert text to audio
     myobj = gTTS(text=text)
     myobj.save("output.mp3")
     os.system("start output.mp3")
-
+#update status lable
     output_label.config(text='conversion completed')
+    #enable the downloade button and exit button
     download_button.config(state='normal')
     exit_button.config(state='normal')
 
 def download_audio():
+     # open file dialog to setect the download location
     download_path = filedialog.asksaveasfilename(defaultextension='.mp3')
     if download_path:
+         # move the output file to the download location
         shutil.move('output.mp3', download_path)
 
 def exit_app():
     if messagebox.askyesno('exit', 'do you want to exit'):
         window.destroy()
 
+# create the main windo
 window = tk.Tk()
 window.title('text to audio converter')
 
+# creat a button for file selection
 select_button = tk.Button(window, text='select file', command=convert_to_audio)
 select_button.pack(pady=15)
 
+# create a lable to display status
 output_label = tk.Label(window, text='')
 output_label.pack()
 
+# create a button for donloading the audio
 download_button = tk.Button(window, text='Download Audio', state='disabled', command=download_audio)
 download_button.pack(pady=15)
 
+#creat an exit button
 exit_button = tk.Button(window, text='Exit', state='disabled', command=exit_app, fg='white', bg='red')
 exit_button.pack(pady=15)
 
+#run the main loop
 window.mainloop()
